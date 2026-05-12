1a8f12d536c41ad00a55dbc198ec48c130aa1500
ankanmal <ankanlogin@gmail.com>
2026-05-12 14:57:27 +0530

    Validation using Yup

--- src/routes/todoRoute.js
+++ src/routes/todoRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createTodo, deleteTodo, getAllTodo, paginateTodo, updateTodo } from '../controllers/todoController.js';
 import { hasToken } from '../middleware/hasToken.js';
+import { todoValidateSchema, validateTodo } from '../validators/todoValidate.js';
 
 const todoRoute = express.Router()
 
-todoRoute.post('/create', hasToken, createTodo)
+todoRoute.post('/create', hasToken, validateTodo(todoValidateSchema), createTodo)
 todoRoute.get('/getAll', hasToken, getAllTodo)
 todoRoute.delete('/deleteTodo/:id', hasToken, deleteTodo)
 todoRoute.patch('/update/:id', hasToken, updateTodo)
