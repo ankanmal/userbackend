18e0441493bc885bdc39d5db1de3b145d6e6352b
ankanmal <ankanlogin@gmail.com>
2026-05-12 12:58:36 +0530

    Multer & Image Upload

--- server.js
+++ server.js
@@ -2,18 +2,20 @@
 
 import express from "express";
 import dbConnect from "./src/config/dbConnect.js";
-import 'dotenv/config'
+import "dotenv/config";
 import userRoute from "./src/routes/userRoute.js";
 import todoRoute from "./src/routes/todoRoute.js";
+import multerRoute from "./src/routes/multerRoute.js";
 
 const app = express();
-const port = process.env.PORT
-dbConnect()
-app.use(express.json())
-app.use('/user', userRoute)
-app.use('/todo', todoRoute)
+const port = process.env.PORT;
+dbConnect();
+app.use(express.json());
+app.use("/upload", express.static("upload"));
+app.use("/user", userRoute);
+app.use("/todo", todoRoute);
+app.use("/multer", multerRoute);
 
-
-app.listen(port,()=>{
-    console.log(`Server Started on Port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Started on Port: ${port}`);
+});
