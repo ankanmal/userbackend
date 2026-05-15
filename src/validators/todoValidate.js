4a0192d55e659e435ae64e6ec9d87007f49752bb
ankanmal <ankanlogin@gmail.com>
2026-05-15 11:59:48 +0530

    Updated Validation for Todo, using a variable to store validated Data

--- src/validators/todoValidate.js
+++ src/validators/todoValidate.js
@@ -12,7 +12,8 @@ export const todoValidateSchema = yup.object({
 
 export const validateTodo = (schema) => async (req, res, next) => {
     try {
-        await schema.validate(req.body);
+        const validatedTodo= await schema.validate(req.body);
+        req.body= validatedTodo
         next();
     } catch (err) {
         return res.status(400).json({ errors: err.errors });
